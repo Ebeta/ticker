9a1bdacb0fabb19de25e2962118799e2a0306e60
Eric Betancourt <eric_betancourt@outlook.com>
2015-01-28 15:21:15 -0500

    just trying to get this to work

---
+++ public/ticker/ticker.rb
@@ -0,0 +1,12 @@
+
+require 'nokogiri'
+require 'httparty'
+require 'json'
+require 'better_errors'
+response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+dom = Nokogiri::HTML(response.body)
+span = dom.xpath("//span[@id='yfs_l84_aapl' ]").first
+price = span.content
+puts "the current price of APPL is $#{price}"
+
+
